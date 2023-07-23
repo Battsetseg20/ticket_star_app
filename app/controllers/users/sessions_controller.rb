930d393c6dc949edbc414f5e0524cc1a1af246a8
Battsetseg20 <tsetseg.yo@gmail.com>
2023-07-23 22:34:55 +0200

    custom signout

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Users::SessionsController < Devise::SessionsController
+    # DELETE /users/sign_out
+    def destroy
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      set_flash_message! :notice, :signed_out if signed_out
+      yield if block_given?
+      respond_to_on_destroy
+    end
+  
+    protected
+  
+    def respond_to_on_destroy
+      # Redirect to the root_path after signing out
+      redirect_to root_path
+    end
+  end
