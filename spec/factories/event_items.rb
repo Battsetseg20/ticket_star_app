ba3672f14effafbcfb40a2e8ba1d98108891d6a0
Battsetseg20 <tsetseg.yo@gmail.com>
2023-07-30 22:33:04 +0200

    small changes in factories

--- spec/factories/event_items.rb
+++ spec/factories/event_items.rb
@@ -1,21 +1,46 @@
 FactoryBot.define do
   factory :event_item do
-    title { Faker::Commerce.product_name }
+    title { Faker::Book.title }
     description { Faker::Lorem.sentence }
     date { Faker::Date.between(from: Date.today, to: 1.year.from_now) }
     time { Faker::Time.between(from: Time.now, to: 1.year.from_now, format: :default) }
     location { Faker::Address.full_address }
     status { EventItem.statuses.keys.sample }
+    event_type { EventItem.event_types.keys.sample }
     event_organizer { association(:event_organizer) }
+  end
 
-    trait :with_ticket do
-      after(:create) do |event_item|
-        create(
-          :ticket,
-          price: Faker::Commerce.price(range: 10..100),
-          quantity_available: Faker::Number.between(from: 1, to: 10),
-          event_item: event_item)
+  trait :with_image do
+    after(:create) do |event_item|
+      image_url = case event_item.event_type
+      when 'literature'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['book'])
+      when 'music'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['music'])
+      when 'sports'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['sports'])
+      when 'food'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['food'])
+      when 'health'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['health'])
+      when 'family'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['family'])
+      when 'education'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['education'])
+      when 'other'
+        Faker::LoremFlickr.image(size: "300x300", search_terms: ['tourism'])
       end
+      event_item.image.attach(io: URI.open(image_url), filename: 'image.png')
+    end 
+  end
+
+  trait :with_ticket do
+    after(:create) do |event_item|
+      create(
+        :ticket,
+        price: Faker::Commerce.price(range: 10..100),
+        quantity_available: Faker::Number.between(from: 1, to: 100),
+        event_item: event_item)
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    encrypted_password { Faker::Internet.unique.username(separators: %w(. _))  }
-    username { Faker::Internet.username(specifier: 6..12) }
+    encrypted_password { "123456789"  } #Faker::Internet.unique.username(separators: %w(. _))
+    username { Faker::Internet.unique.username(specifier: 6..12) }
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
     birthdate { Faker::Date.birthday(min_age: 18, max_age: 65)  }
