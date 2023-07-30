d5d16e1412b34efea77e685d3441ff448241bf09
Battsetseg20 <tsetseg.yo@gmail.com>
2023-07-30 22:30:54 +0200

    display events by type in welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @event_items = EventItem.published.order(date: :desc).group_by(&:event_type)
+    fixed_order = EventItem.event_types.keys
+    @event_items = @event_items.sort_by { |key, _| fixed_order.index(key) }.to_h
+
+    # Take the last 4 event items for the welcome board cards
+    @event_items = @event_items.transform_values { |event_items| event_items.last(4) }
+
+  end
+
+  def disclaimer; end
 end
