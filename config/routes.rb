98102fcabcfb301e3173a1cb19ac29abb5cb62cd
Battsetseg20 <tsetseg.yo@gmail.com>
2023-07-30 22:34:21 +0200

    schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "welcome#index"
+  get "/disclaimer", to: "welcome#disclaimer", as: :disclaimer
+ 
   # Devise routes for User
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
-
+  # Custom registration routes for User (customer and event organizer)
   devise_scope :user do
     get "/register/customer", to: "users/registrations#new_customer", as: :new_customer_registration
     get "/register/event_organizer", to: "users/registrations#new_event_organizer",
@@ -15,7 +18,16 @@ Rails.application.routes.draw do
     post "/register/event_organizer", to: "users/registrations#create", defaults: { user_type: "event_organizer" }
   end
 
-  resources :event_items, only: [:new, :create, :edit, :update, :show, :index, :destroy]
+  resources :event_items, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
+    collection do
+      get 'by_type/:event_type', action: :by_type, as: 'by_type'
+      get 'hot_events', action: :hot_events, as: 'hot_events'
+    end
+    # TODO: will only add later if enough time to implement some kind of reviewer update job that
+    # updated the review score of the event item once new reviews are added or old reviews are deleted
+    # resources :reviews, only: [:create, :destroy]
+  end
+  
   resources :purchases, only: [:show] do
     collection do
       get 'buy_tickets', to: 'purchases#create'
@@ -28,6 +40,4 @@ Rails.application.routes.draw do
 
   # add route for customer dashboard
   # add route for event organizer dashboard
-
-  root to: "welcome#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_143803) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_144724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_143803) do
     t.uuid "event_organizer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "event_type", default: 0
     t.index ["event_organizer_id"], name: "index_event_items_on_event_organizer_id"
   end
 
