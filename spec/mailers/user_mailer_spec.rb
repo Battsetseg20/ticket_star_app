3ff4e14fdcfe99462b0a6f73380a485e31af291b
Battsetseg20 <tsetseg.yo@gmail.com>
2023-07-31 03:36:50 +0200

    mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#ticket_email" do
+    let(:purchase) { FactoryBot.create(:purchase) }
+    let(:mail) { UserMailer.with(purchase: purchase, pdf_file: "spec/fixtures/files/sample_ticket.pdf").ticket_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Your Ticket Purchase Confirmation")
+      expect(mail.to).to eq([purchase.customer.user.email])
+      expect(mail.from). to eq(["from@ticketstar.com"])
+    end
+  end
 end
